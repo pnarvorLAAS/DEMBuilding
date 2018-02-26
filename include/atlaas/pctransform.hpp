92af1c33e517dd5e99ef0b324fb7823c7206db12
Quentin Labourey <quentin.labourey@laas.fr>
2018-02-26 08:05:30 +0100

    (Splitted from CDFF_at_LAAS)[ATLAAS] Added pctransform DFN header file [DEMBuidling] Generated PCTrasnform DFN with Dfn_generator

---
+++ include/atlaas/pctransform.hpp
@@ -0,0 +1,24 @@
+#ifndef _PCTRANSFORM_HPP_
+#define _PCTRANSFORM_HPP_
+
+#include <PointCloudPoseStamped.h>
+#include <atlaas/common.hpp>
+
+namespace atlaas {
+
+class cloudTransform
+{
+    private:
+        points pointCloud;
+        matrix tfSensor2World;
+        PointCloudPoseStamped pointCloudMsg;
+
+    public:
+        bool decode_message(BitStream &msg);
+        bool update_transform(/*pointCloudMsg,tfSensor2World*/);
+        bool update_pointCloud(/*pointCloudMsg,pointCloud*/);
+        bool transform_pointCloud(/*pointCloud*/);
+};
+
+
+#endif
---
+++ src/pctransform.cpp
@@ -0,0 +1,25 @@
+#ifndef _PCTRANSFORM_CPP
+#define _PCTRANSFORM_CPP
+
+namespace atlaas{
+
+    bool cloudTransform::decode_message(BitStream &msg)
+    {
+        return true;
+    }
+
+    bool update_transform(/*pointCloudMsg,tfSensor2World*/)
+    {
+        return true;
+    }
+    bool update_pointCloud(/*pointCloudMsg,pointCloud*/)
+    {
+        return true;
+    }
+    bool transform_pointCloud(/*pointCloud*/)
+    {
+        return true;
+    }
+}
+
+#endif
