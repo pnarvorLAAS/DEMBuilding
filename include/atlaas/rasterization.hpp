3871837a7f72a5e68633d67c992ec585afff1050
Quentin Labourey <quentin.labourey@laas.fr>
2018-02-28 11:14:02 +0100

    (Splitted from CDFF_at_LAAS)[ATLAAS] Filled rasterization.hpp: need an ASN file for DEM

--- include/atlaas/rasterization.hpp
+++ include/atlaas/rasterization.hpp
@@ -2,10 +2,44 @@
 #define __RASTERIZATION_HPP__
 
 #include <PointCloudPoseStamped.h>
+#include <gdalwrap/gdal.h>
 #include <common.hpp>
 
 class pcRasterizer{
-    
+    private:
+
+        /* Inputs and outputs of the DFN */
+        points                  pointCloud;
+        matrix                  tfSensor2World;
+        PointCloudPoseStamped   pcMsgInput;
+        // demRaster            demMsgOutPut;
+
+        /* Internal variables */
+
+        gdalwrap::gdal          meta;
+        cells_info_t            dnyinter;
+
+        point_xy_t              sensor_xy;
+
+        size_t                  width;
+        size_t                  height;
+        int                     sw;
+        int                     sh;
+
+
+
+    public:
+
+        bool decode_message(BitStream msg);
+        void init(double size_x,double size_y, double scale,
+                  double custom_x, double custom_y, double custom_z,
+                  int utm_zone, boot utm_north=true);
+        bool rasterize(/*pointCloud*/);
+        void do_slide();
+        bool slide(/*sensor_xy, meta*/);
+        BitStream encode_message(/*demMsgOutput*/);
+
+
 };
 
 #endif
