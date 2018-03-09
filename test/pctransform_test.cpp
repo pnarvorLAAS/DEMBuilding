0683d49ff3639c4236aaab33672370e9da9f6a6d
Quentin Labourey <quentin.labourey@laas.fr>
2018-03-09 17:39:54 +0100

    (Splitted from CDFF_at_LAAS)[ATLAAS][TEST] Added a test for PCTransform

---
+++ test/pctransform_test.cpp
@@ -0,0 +1,13 @@
+#undef NDEBUG
+#include <iostream>
+#include <atlaas/pctransform.hpp>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int k;
+    atlaas::cloudTransform test;
+    cin >> k;
+    return 0;
+}
